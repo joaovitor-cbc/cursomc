5a8cebcae37b3407acdad210bb962c6f7c926bd2
JoaoVitor <joaovitor.novacruz@gmail.com>
2021-09-27 15:04:47 -0300

    Salvando perfis de usuario na base de dados

--- src/main/java/com/example/cursomc/services/DBService.java
+++ src/main/java/com/example/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.example.cursomc.domain.PagamentoComCartao;
 import com.example.cursomc.domain.Pedido;
 import com.example.cursomc.domain.Produto;
 import com.example.cursomc.domain.enums.EstadoPagamento;
+import com.example.cursomc.domain.enums.Perfil;
 import com.example.cursomc.domain.enums.TipoCliente;
 import com.example.cursomc.repositories.CategoriaRepository;
 import com.example.cursomc.repositories.CidadeRepository;
@@ -133,18 +134,25 @@ public class DBService {
 		Cliente cli1 = new Cliente(null, "Maria Silva", "joaovitor.novacruz@gmail.com",
 				"36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		
+
+		Cliente cli2 = new Cliente(null, "Ana Costa", "joaovitor.novacruz@hotmail.com", "31628382740", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.getTelefones().addAll(Arrays.asList("93883321", "34252625"));
+		cli2.addPerfil(Perfil.ADMIN);
+		
 		//criando Telefones
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
 		//criando Endereco
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2106", null, "Centro", "281777012", cli2, c2);
 		
 		//associacao de enderenco em cliente
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 		
 		//criando mascara para data e hora
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
