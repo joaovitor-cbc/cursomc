823370bf6e8832a01672b11421550e1823342ab1
JoaoVitor <joaovitor.novacruz@gmail.com>
2021-09-28 14:58:40 -0300

    Esqueci a senha

--- src/main/java/com/example/cursomc/resources/AuthResource.java
+++ src/main/java/com/example/cursomc/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package com.example.cursomc.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.cursomc.dto.EmailDTO;
 import com.example.cursomc.security.JWTUtil;
 import com.example.cursomc.security.UserSS;
+import com.example.cursomc.services.AuthService;
 import com.example.cursomc.services.UserService;
 
 @RestController
@@ -18,6 +22,9 @@ public class AuthResource {
 
 	@Autowired
 	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private AuthService authService;
 
 	//atualizar token
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
@@ -25,6 +32,12 @@ public class AuthResource {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+		authService.sendNewPassword(objDto.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/example/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/example/cursomc/services/AbstractEmailService.java
@@ -13,6 +13,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.example.cursomc.domain.Cliente;
 import com.example.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService {
@@ -31,7 +32,7 @@ public abstract class AbstractEmailService implements EmailService {
 		SimpleMailMessage sm = prepareSimpleMailMessageFromPedido(obj);
 		sendEmail(sm);
 	}
-
+	
 	protected SimpleMailMessage prepareSimpleMailMessageFromPedido(Pedido obj) {
 		SimpleMailMessage sm = new SimpleMailMessage();
 		//para quem vai o email
@@ -76,5 +77,24 @@ public abstract class AbstractEmailService implements EmailService {
 		mmh.setSentDate(new Date(System.currentTimeMillis()));
 		mmh.setText(htmlFromTemplatePedido(obj), true);
 		return mimeMessage;
+	}
+	
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
+		sendEmail(sm);
+	}
+
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		//para quem vai o email
+		sm.setTo(cliente.getEmail());
+		//quem ta mandando
+		sm.setFrom(sender);
+		sm.setSubject("Solicitação e nova senha");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		//corpo do email
+		sm.setText("Nova senha: " + newPass);
+		return sm;
 	}
 }
